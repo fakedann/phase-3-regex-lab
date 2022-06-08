9ec02a2ef4e3d568ad84e1413e40fbbdcaf874bc
Daniel Escalona <daniel07escalona@gmail.com>
2022-06-08 17:46:59 -0400

    commit tree a9455981589f43739c00b005ea7bea1cffe6db80

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,15 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/\b[aeiouAEIOU]/) == []
+    return false
+  end
+  true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  new_arr = text.scan(/\bun\w*ing\b/)
 end
+
+words_starting_with_un_and_ending_with_ing("unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary")
 
 def words_five_letters_long(text)
 
